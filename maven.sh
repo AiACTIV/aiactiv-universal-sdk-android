d1431254cf823879e9e07cce0e83e0d080edbc6f
Thai Duy Cuong <thaiduycuong@Tdc.local>
2023-07-05 18:16:22 +0700

    Add script for maven install

---
+++ maven.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the desired Maven version
+maven_version=$1
+
+# Define the download URL
+download_url="https://apache.osuosl.org/maven/maven-3/${maven_version}/binaries/apache-maven-${maven_version}-bin.tar.gz"
+
+# Download Maven
+echo Downloading...$download_url
+curl -fsSL "$download_url" -o tmp/apache-maven.tar.gz
+#wget "$download_url" tmp/apache-maven.tar.gz
+
+# Extract Maven to a temporary directory
+tar xf tmp/apache-maven.tar.gz -C tmp
+
+# Set up environment variables
+export MAVEN_HOME="tmp/apache-maven-${maven_version}"
+export PATH="$MAVEN_HOME/bin:$PATH"
+
+# Verify the installation
+mvn --version
